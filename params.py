7dd82396114c6ecbf6130fb35f3f233763a0482c
vchaffraix <valentin.chaffraix@etu.univ-lyon1.fr>
2020-01-21 18:04:52 +0100

    modifs params

--- params.py
+++ params.py
@@ -3,9 +3,11 @@ import string
 GROUP_ID = 31
 PASS_LENGTH = range(12,19)
 CHARS = string.ascii_uppercase + string.digits
-N = 200
+N = 100
 MAXGEN = 1000
-e = 5
+e = 12
 p_cross = 0.6
-p_mut = 0.05
+p_mut = 0.3
 C_max = 1
+# "wheel" ou "tournament"
+SELECT_FUNCTION = "wheel"
