98b09a81f36a81f7b56cee465ff630c688b4f7b1
vchaffraix <valentin.chaffraix@etu.univ-lyon1.fr>
2020-01-25 19:08:47 +0100

    Ajout min et max showresults

--- show-results.py
+++ show-results.py
@@ -16,17 +16,24 @@ for res in results:
     for key in res["params"]:
         print("\t* "+ key + ":" + str(res["params"][key]))
     print("-----------------------RÉSULTAT----------------------")
+    min_n = 1000
+    max_n = 0
     sum_n = 0
     sum_time = 0
     n_found = 0
     password = None
     for r in res["results"]:
-        sum_n += r["n_generations"]
+        n = r["n_generations"]
+        max_n = max(n, max_n)
+        min_n = min(n, min_n)
+        sum_n += n
         sum_time += r["time"]
         if(r["found"]):
             password = r["password"]
             n_found += 1
+    print("Nombre minimum de génération : " + str(min_n))
     print("Nombre moyen de génération : " + str(round(sum_n/n_found,2)))
+    print("Nombre maximum de génération : " + str(max_n))
     print("Temps moyen d'exécution de l'algorithme : " + str(round(sum_time/n_found,2)) + "s")
     print("Mot de passe trouvé : " + str(n_found) + "/" + str(len(res["results"])))
     print("Mot de passe : " + password)
